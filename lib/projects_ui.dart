68b739bf03a18f7aa0eb71f831ab066837562c73
Andre Haliim <andre@infonetglobal.tech>
2025-03-12 12:18:12 +0700

    finalize projects

--- lib/projects_ui.dart
+++ lib/projects_ui.dart
@@ -5,6 +5,68 @@ import 'package:portfolio/constants.dart';
 class ProjectsUI extends StatelessWidget {
   ProjectsUI({super.key});
 
+  final String titleAFC = 'AutoFleet Capital';
+  final String titleFIKA = 'FIKA';
+  final String titleGTS = 'Gold Trading System (GTS)';
+  final String titleVTS = 'Vehicle Tracking System (VTS)';
+
+  final List<String> afcImage = [
+    'assets/images/afc/afc1.jpg',
+    'assets/images/afc/afc2.jpg',
+    'assets/images/afc/afc3.jpg',
+    'assets/images/afc/afc4.jpg',
+    'assets/images/afc/afc5.jpg',
+    'assets/images/afc/afc6.jpg',
+  ];
+  final List<String> fikaImage = [
+    'assets/images/fika/fika1.jpg',
+    'assets/images/fika/fika2.jpg',
+    'assets/images/fika/fika3.jpg',
+    'assets/images/fika/fika4.jpg',
+    'assets/images/fika/fika5.jpg',
+    'assets/images/fika/fika6.jpg',
+  ];
+  final List<String> gtsImage = [
+    'assets/images/gts/gts1.jpg',
+    'assets/images/gts/gts2.jpg',
+    'assets/images/gts/gts3.jpg',
+    'assets/images/gts/gts4.jpg',
+    'assets/images/gts/gts5.jpg',
+    'assets/images/gts/gts6.jpg',
+  ];
+  final List<String> vtsImage = [
+    'assets/images/vts/vts1.jpg',
+    'assets/images/vts/vts2.jpg',
+    'assets/images/vts/vts3.jpg',
+    'assets/images/vts/vts4.jpg',
+    'assets/images/vts/vts5.jpg',
+    'assets/images/vts/vts6.jpg',
+  ];
+
+  final String descriptionAFC = 'This application is designed to streamline the auction process for AutoFleet Capital customers,'
+      ' enabling seamless transactions from start to finish.'
+      ' Users can check auction stock, place bids, monitor auction progress,'
+      ' and even recommend vehicles for auction to other users within the platform.';
+
+  final String descriptionFIKA = 'This application streamlines the inspection process for both field operations and on-site conditions.'
+      ' Utilizing a questionnaire-based method, administrators can create customized questions related to field activities,'
+      ' which are then completed by designated users. In the event of any issues encountered during the questionnaire submission,'
+      ' users can report them directly through the "Issue" section. These reports are immediately visible to administrators for'
+      ' further review and follow-up actions from the central office. Additionally, the application provides comprehensive reporting'
+      ' features, enabling administrators to monitor and evaluate the questionnaire results submitted by each staff member.';
+
+  final String descriptionGTS = 'This application serves as a tool to streamline gold sales transactions for field sales representatives.'
+      ' By utilizing a synchronization system with the server and storing product data in the applications local database,'
+      ' sales personnel can operate the application offline, ensuring seamless transactions even in areas with limited or no network'
+      ' coverage. The transactions facilitated by the app include gold sales and payments, which can be made either with gold or cash.'
+      ' Additionally, the application features comprehensive reporting capabilities, allowing management to monitor and evaluate the'
+      ' sales performance of each representative.';
+
+  final String descriptionVTS = 'This application is designed to monitor the delivery transactions of a vehicles from one'
+      ' location to another. It works in synergy with tracking devices installed on each vehicle, with real-time location data'
+      ' recorded by the server and displayed within the application. The app also features a barcode scanning function, enabling'
+      ' users to easily register trackers and vehicles for location monitoring, ensuring a more efficient and streamlined process.';
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -34,29 +96,72 @@ class ProjectsUI extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ProjectItem(
-                  imagePath: 'assets/images/afc1.jpg',
-                  screenWidth: screenWidth,
-                  screenHeight: screenHeight,
-                  imageTitle: 'AutoFleet Capital Mobile App',
+                Column(
+                  children: [
+                    ProjectItem(
+                      imagePath: 'assets/images/afc/afc1.jpg',
+                      screenWidth: screenWidth,
+                      screenHeight: screenHeight,
+                      imageTitle: titleAFC,
+                      imageDescription: descriptionAFC,
+                      imageList: afcImage,
+                    ),
+                    Text(
+                      titleAFC,
+                      style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.01),
+                    ),
+                  ],
                 ),
-                ProjectItem(
-                  imagePath: 'assets/images/fika1.jpg',
-                  screenWidth: screenWidth,
-                  screenHeight: screenHeight,
-                  imageTitle: 'FIKA Mobile App',
+                SizedBox(width: screenWidth * 0.05,),
+                Column(
+                  children: [
+                    ProjectItem(
+                      imagePath: 'assets/images/fika/fika1.jpg',
+                      screenWidth: screenWidth,
+                      screenHeight: screenHeight,
+                      imageTitle: titleFIKA,
+                      imageDescription: descriptionFIKA,
+                      imageList: fikaImage,
+                    ),
+                    Text(
+                      titleFIKA,
+                      style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.01),
+                    ),
+                  ],
                 ),
-                ProjectItem(
-                  imagePath: 'assets/images/gts1.jpg',
-                  screenWidth: screenWidth,
-                  screenHeight: screenHeight,
-                  imageTitle: 'Gold Trading System',
+                SizedBox(width: screenWidth * 0.05,),
+                Column(
+                  children: [
+                    ProjectItem(
+                      imagePath: 'assets/images/gts/gts1.jpg',
+                      screenWidth: screenWidth,
+                      screenHeight: screenHeight,
+                      imageTitle: titleGTS,
+                      imageDescription: descriptionGTS,
+                      imageList: gtsImage,
+                    ),
+                    Text(
+                      titleGTS,
+                      style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.01),
+                    ),
+                  ],
                 ),
-                ProjectItem(
-                  imagePath: 'assets/images/vts1.jpg',
-                  screenWidth: screenWidth,
-                  screenHeight: screenHeight,
-                  imageTitle: 'Vehicle Tracking System',
+                SizedBox(width: screenWidth * 0.05,),
+                Column(
+                  children: [
+                    ProjectItem(
+                      imagePath: 'assets/images/vts/vts1.jpg',
+                      screenWidth: screenWidth,
+                      screenHeight: screenHeight,
+                      imageTitle: titleVTS,
+                      imageDescription: descriptionVTS,
+                      imageList: vtsImage,
+                    ),
+                    Text(
+                      titleVTS,
+                      style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.01),
+                    ),
+                  ],
                 ),
               ],
             )
@@ -70,6 +175,8 @@ class ProjectItem extends StatefulWidget {
   final double screenWidth;
   final double screenHeight;
   final String imageTitle;
+  final String imageDescription;
+  final List<String> imageList;
 
   const ProjectItem({
     super.key,
@@ -77,6 +184,8 @@ class ProjectItem extends StatefulWidget {
     required this.screenWidth,
     required this.screenHeight,
     required this.imageTitle,
+    required this.imageDescription,
+    required this.imageList,
   });
 
   @override
@@ -91,18 +200,6 @@ class _ProjectItemState extends State<ProjectItem> {
       _isHovered = hovering;
     });
   }
-
-  final List<String> afcImage = [
-    'assets/images/afc1.jpg',
-    'assets/images/fika1.jpg',
-    'assets/images/gts1.jpg',
-    'assets/images/vts1.jpg',
-  ];
-
-  String descriptionAFC = 'This application is designed to streamline the auction process for AutoFleet Capital customers,'
-      ' enabling seamless transactions from start to finish.'
-      ' Users can check auction stock, place bids, monitor auction progress,'
-      ' and even recommend vehicles for auction to other users within the platform.';
 
   @override
   Widget build(BuildContext context) {
@@ -137,7 +234,7 @@ class _ProjectItemState extends State<ProjectItem> {
             ),
           if (_isHovered)
             ElevatedButton(
-              onPressed: () => _showCustomDialog(context, widget.imageTitle, afcImage, descriptionAFC),
+              onPressed: () => _showCustomDialog(context, widget.imageTitle, widget.imageList, widget.imageDescription),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.white,
                 foregroundColor: Colors.black,
@@ -197,7 +294,7 @@ class _ProjectItemState extends State<ProjectItem> {
                   description,
                   style: TextStyle(color: Colors.black, fontSize: screenSize.width * 0.01),
                 ),
-                SizedBox(height: screenSize.height / 15,),
+                Spacer(),
                 Text(
                   'Platform : Android & iOS',
                   style: TextStyle(color: Colors.black, fontSize: screenSize.width * 0.01),
